70d9f02a90fd4113b306c6151c3accdaa4b2bac1
thegooz45 <110310482+thegooz45@users.noreply.github.com>
2022-10-18 10:09:04 -0400

    adding python files!
    from my "100 days of code" course

---
+++ pythonCode/1-band-name-generator.py
@@ -0,0 +1,14 @@
+# day 1: band name generator
+# doing this with 100 days of code for python on udemy
+# done by me: thegooz45 
+
+#1. Create a greeting for your program.
+print("hello! welcome to band name generator!")
+
+#2. Ask the user for the city that they grew up in.
+cityName = input("what city did you grow up in? ")
+
+#3. Ask the user for the name of a pet.
+petName = input("and what was the name of the pet you had growing up? ")
+#4. Combine the name of their city and pet and show them their band name.
+print("based on the info you gave me, your band name would be: " + cityName + " " + petName)
